fed5e49744eae73c881b1ca9428c05bc042ade2a
Patryk <patryk.kodehode@gmail.com>
2022-12-06 11:01:33 +0100

    Code and comments from lecture2

--- js/index.js
+++ js/index.js
@@ -10,14 +10,14 @@ const errorElement = document.querySelector(".form-error")
 
 // local storage key:
 const localStorageKey = "todolist"
-// array containing all todolist items:
-let todolistItems = []
 
-// load items from localStorage:
-todolistItems = JSON.parse(
-  localStorage.getItem(localStorageKey)
-)
+// || offers an alternative way to declare and conditionally assign a variable (see comments below all code, lines: 80+ for more info)
+let todolistItems = JSON.parse( localStorage.getItem(localStorageKey) ) || []
 
+// loop through todolistItems array and create todolist item html elements for each of them
+todolistItems.forEach((todoItem) => {
+  createTodoListItem(todoItem)
+})
 
 // add click event listener to createNoteButton
 createNoteButton.addEventListener("click", function() {
@@ -31,19 +31,28 @@ createNoteButton.addEventListener("click", function() {
   // reset error element to empty string:
   errorElement.textContent = ""
 
+  // create an object with an itemname and a unique id
+  const newTodoItem = {itemName: inputText, id: Date.now()}
   // append new todolist item to todolistItems arrays
-  todolistItems.push(inputText)
+  todolistItems.push(newTodoItem)
 
   // add item to local storage:
   localStorage.setItem(localStorageKey, JSON.stringify(todolistItems))
 
+  // create html elements for the new todolist item
+  createTodoListItem(newTodoItem)
+  
+})
+
+// creates html elements for a new todolist item
+function createTodoListItem(todoItem) {
   // create todoItemWrapper
   const todoItemWrapper = document.createElement("div")
   todoItemWrapper.className = "todo-item"
   // create todoItem text
   const todoItemText = document.createElement("div")
   todoItemText.className = "todo-item-text"
-  todoItemText.textContent = inputText
+  todoItemText.textContent = todoItem.itemName
   // create todoItem delete button
   const todoItemDeleteButton = document.createElement("button")
   todoItemDeleteButton.textContent = "❌"
@@ -51,74 +60,44 @@ createNoteButton.addEventListener("click", function() {
   // add event listener to the ❌ button: 
   todoItemDeleteButton.addEventListener("click", () => {
     todoItemWrapper.remove()
+
+    // remove item matching "todoText" from todolistItems array: (see lines 98 for a more readable longer version of the same code as below line)
+    todolistItems = todolistItems.filter(todoListItem => todoListItem.id !== todoItem.id)
+    // update local storage:
+    localStorage.setItem(localStorageKey, JSON.stringify(todolistItems))
   })
 
   // append the todoItemText, todoItemDeleteButton to the todoItemWrapper
   todoItemWrapper.append(todoItemText, todoItemDeleteButton)
-
   // append todoItemWrapper to the todolistWrapper
   todolistWrapper.append(todoItemWrapper)
-})
-
-console.log(localStorage.getItem(localStorageKey))
-
-
-// local storage:
-//let todolistcount = 1
-//localStorage.clear()
-
-// let todoListArray = ["code", "testcode", "testmorecode"]
-
-//console.log(todoListArray[1])
-
-// convert Javascript array to JSON string:
-// let todoListAsJSON = JSON.stringify(todoListArray)
-
-// convert JSON to javascript object / array:
-// let todoListAsArray = JSON.parse(todoListAsJSON)
-
-// console.log(todoListAsArray)
-
-
-// create item in local storage:
-//localStorage.setItem("todolist", todoListArray)
-
-// read contents from local storage:
-//let temporaryStorage = localStorage.getItem("todolist")
-
-// console.log(typeof temporaryStorage)
+}
 
 
 
 
 
+// Explanation about this code: let todolistItems = JSON.parse( localStorage.getItem(localStorageKey) ) || []
 
+// As mentioned during lecture, that code does exactly the same thing as this code would:
 
+// first declare an empty array:
+// let todolistItems = []
+// then set the array to data from local storage:
+// let todolistItems = JSON.parse( localStorage.getItem(localStorageKey) )
+// check if data from localstorage was empty, if it was set it back to an empty array:
+// if (todolistItems == null) todolistItems = []
 
+// The reason this works is because the || checks if the statement on the left side is falsy, and if it is it will use whatever is on the right side
+// console.log(false || "something") // will show "something"
+// console.log(false || "") // will show <empty string>
+// console.log(true || []) // will show true
 
+// for a technical explanation see here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
 
 /*
-
-// temporary counter:
-let count = 0
-
-
-// innerHTML problem #1: users can insert html tags, or even run javascript
-// <img src="a@14@%^" onerror="alert('Hacked');">
-// innerHTML problem #2: it removes any existing event listeners from all child-elements the container it is attached to
-
-  todolistWrapper.innerHTML += `
-    <div class="todo-item" id="todo-id-${count}">
-      <div class="todo-item-text">
-        ${inputText}
-      </div>
-      <button id="button-delete-${count}">❌</button>
-    </div>`;
-
-  // add event listener to the ❌ button:
-  const todoButtonDelete = document.getElementById(`button-delete-${count}`)
-  todoButtonDelete.addEventListener("click", function() {
-    const todoItemElement = document.getElementById(`todo-id-${count}`)
-    todoItemElement.remove()
-  })
+todolistItems = todolistItems.filter(function(todoListItem) {
+  if (todoListItem.id !== todoItem.id) return true
+  else return false
+})
 */
